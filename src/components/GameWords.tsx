42e0977ae78d49228bb824ec9827fce5a84959aa
Ana Popovic <franjetina@gmail.com>
2024-04-11 11:09:38 +0200

    feat: guess the letter style added

--- src/components/GameWords.tsx
+++ src/components/GameWords.tsx
@@ -3,9 +3,20 @@ interface GameWordsProps {
   }
   
 export const GameWords: React.FC<GameWordsProps> = ({ guessWord }) => {
+    const guessedLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
     return (
-      <div>
-        The guessed word is: {guessWord}
+      <div className="words">
+        {guessWord.split('').map((letter, index) => (
+          <span key={index} className="letters">
+            <span 
+                style={{
+                visibility: guessedLetter.includes(letter) ? 'visible' : 'hidden'
+            }}
+            >
+                {letter}
+            </span>
+          </span>
+        ))}
       </div>
     );
   };
