bfcc93cf0c4e2ca44eb9541d3c006d94d35c2697
Ana Popovic <franjetina@gmail.com>
2024-04-11 22:06:13 +0200

    feat: keyboard logic added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import React, { useState } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import words from './common.json'
 import { GameDrawing } from './components/GameDrawing';
 import { GameWords } from './components/GameWords';
@@ -15,15 +15,78 @@ function App() {
     return commonWordsArray[randomIndex];
   });
 
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  const incorrectLetters = guessedLetters.filter(
+    letter => !guessWord.includes(letter)
+  )
+
+  const isLoser = incorrectLetters.length >= 6
+  const isWinner = guessWord
+    .split("")
+    .every(letter => guessedLetters.includes(letter))
+
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (guessedLetters.includes(letter) || isLoser || isWinner) return
+
+      setGuessedLetters(currentLetters => [...currentLetters, letter])
+    },
+    [guessedLetters, isWinner, isLoser]
+  )
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+      if (!key.match(/^[a-z]$/)) return
+
+      e.preventDefault()
+      addGuessedLetter(key)
+    }
+
+    document.addEventListener("keypress", handler)
+
+    return () => {
+      document.removeEventListener("keypress", handler)
+    }
+  }, [guessedLetters, addGuessedLetter])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+      if (key !== "Enter") return
+
+      e.preventDefault()
+      setGuessedLetters([])
+      setGuessWord(guessWord)
+    }
+
+    document.addEventListener("keypress", handler)
+
+    return () => {
+      document.removeEventListener("keypress", handler)
+    }
+  }, [])
+
 
   return (
     <>
       <div className='game-container'>
         <h1>Hangman</h1>
         <div className='message'>Win/Lose</div>
-        <GameDrawing />
-        <GameWords guessWord={guessWord} /> 
-        <GameKeyboard />
+        <GameDrawing numberOfGuessedLetters={incorrectLetters.length} />
+        <GameWords 
+          guessWord={guessWord}
+          guessedLetters={guessedLetters}
+         /> 
+        <GameKeyboard
+          // disabled={isWinner || isLoser}
+            activeLetters={guessedLetters.filter(letter =>
+            guessWord.includes(letter)
+          )}
+          inactiveLetters={incorrectLetters}
+          addGuessedLetter={addGuessedLetter}
+           />
       </div>
     </>
   )
--- src/components/GameKeyboard.tsx
+++ src/components/GameKeyboard.tsx
@@ -1,12 +1,21 @@
-export function GameKeyboard() {
+type GameKeyboardProps = {
+    activeLetters: string[]
+    inactiveLetters: string[]
+    addGuessedLetter: (letter: string) => void
+};
+
+export function GameKeyboard({ activeLetters, inactiveLetters, addGuessedLetter }: GameKeyboardProps) {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
   return (
     <div className='keyboard-container'>
-      {alphabet.map((letter) => (
-        <button key={letter} className='keyboard-key'>
-          {letter}
+      {alphabet.map((key) => {
+        // const isActive = activeLetters.includes(letter)
+        // const isInactive = inactiveLetters.includes(letter)
+        return (
+        <button onClick={() => addGuessedLetter(key)} key={key} className='keyboard-key'>
+          {key}
         </button>
-      ))}
+      )})}
     </div>
   );
 }
