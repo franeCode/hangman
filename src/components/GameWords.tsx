038aa393056c9ff5e443008670a3aaeb8fe4e18a
Ana Popovic <franjetina@gmail.com>
2024-04-12 12:02:20 +0200

    feat: revealing word after user failed added

--- src/components/GameWords.tsx
+++ src/components/GameWords.tsx
@@ -1,23 +1,27 @@
-interface GameWordsProps {
+type GameWordsProps = {
     guessWord: string;
     guessedLetters: string[];
+    reveal?: boolean;
   }
   
-export const GameWords: React.FC<GameWordsProps> = ({ guessWord, guessedLetters }) => {
-    // const guessedLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
+export function GameWords({ guessWord, guessedLetters, reveal=false }: GameWordsProps) {
     return (
       <div className="words">
         {guessWord.split('').map((letter, index) => (
           <span key={index} className="letters">
             <span 
                 style={{
-                visibility: (guessedLetters && guessedLetters.includes(letter)) ? 'visible' : 'hidden'
-            }}
-            >
+                  visibility: (guessedLetters && guessedLetters.includes(letter)) || reveal
+                  ? "visible"
+                  : "hidden",
+                  color:
+                    !guessedLetters.includes(letter) && reveal ? "red" : "black",
+                  }}
+                >
                 {letter}
             </span>
           </span>
         ))}
       </div>
     );
-  };
+  }
