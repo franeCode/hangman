24951b62da0d8a19888d12f501fe3a64d8a2ba76
Ana Popovic <franjetina@gmail.com>
2024-04-12 12:01:50 +0200

    active and inactive buttons added

--- src/components/GameKeyboard.tsx
+++ src/components/GameKeyboard.tsx
@@ -2,17 +2,23 @@ type GameKeyboardProps = {
     activeLetters: string[]
     inactiveLetters: string[]
     addGuessedLetter: (letter: string) => void
+    disabled?: boolean
 };
 
-export function GameKeyboard({ activeLetters, inactiveLetters, addGuessedLetter }: GameKeyboardProps) {
+export function GameKeyboard({ activeLetters, inactiveLetters, addGuessedLetter, disabled=false }: GameKeyboardProps) {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
   return (
     <div className='keyboard-container'>
       {alphabet.map((key) => {
-        // const isActive = activeLetters.includes(letter)
-        // const isInactive = inactiveLetters.includes(letter)
+        const isActive = activeLetters.includes(key)
+        const isInactive = inactiveLetters.includes(key)
         return (
-        <button onClick={() => addGuessedLetter(key)} key={key} className='keyboard-key'>
+        <button 
+          onClick={() => addGuessedLetter(key)} 
+          key={key} 
+          className={`keyboard-key ${isActive ? 'keyboard-key.active' : ""} ${isInactive ? 'keyboard-key.inactive' : ""}`}
+          disabled={isInactive || isActive || disabled}
+          >
           {key}
         </button>
       )})}
