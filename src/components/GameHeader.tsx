5039152b3f4548e66d29141f9bbc7893dd35ba64
Ana Popovic <franjetina@gmail.com>
2024-04-24 13:03:42 +0200

    reset game and messages removed from header

--- src/components/GameHeader.tsx
+++ src/components/GameHeader.tsx
@@ -1,26 +1,20 @@
-import logo from '../assets/img/logo.svg';
+import logo from "../assets/img/ap_logo.svg";
 
 type GameHeaderProps = {
-    isWinner: boolean;
-    isLoser: boolean;
-    startNewGame: () => void;
+    // isWinner: boolean;
+    // isLoser: boolean;
+    // startNewGame: () => void;
     language: string;
     handleLanguageChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
-export function GameHeader({ isWinner, isLoser, startNewGame, language, handleLanguageChange}: GameHeaderProps) {
+export function GameHeader({ language, handleLanguageChange}: GameHeaderProps) {
     return (
         <div className='navbar'>
             <div className='logo'>
-                <h1 className='text-chalk-effect logo-chalk-effect'>Hangman</h1>
+                <img src={logo} alt="logo"/>
+                <h1 className='text-chalk-effect'>Hangman</h1>
             </div>
-            <div className='message text-chalk-effect'>
-                {isWinner && "Winner! - Click to try again"}
-                {isLoser && "Nice Try - Click to try again"}
-            </div>
-            <button className='reset-game text-chalk-effect' onClick={startNewGame}>
-                Start New Game
-            </button>
             <div>
                 <select className='languages chalk-effect' value={language} onChange={handleLanguageChange}>
                     <option value="english">🇺🇸 English</option>
