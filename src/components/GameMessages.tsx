32bcbd25b931d8a89689468150bd2dea07f9fc2b
Ana Popović <104695146+franeCode@users.noreply.github.com>
2025-07-30 15:32:23 +0200

    fix keyboard input case and typo

--- src/components/GameMessages.tsx
+++ src/components/GameMessages.tsx
@@ -7,7 +7,7 @@ export function GameMessages({ isWinner, isLoser }: GameMessages) {
     return (
         <div className="message-container">
             {isWinner ? (<p className="message text-chalk-effect">Winner!</p>) : (<p></p>)}
-            {isLoser ? (<p className="message text-chalk-effect">Looser!</p>) : (<p></p>)}
+            {isLoser ? (<p className="message text-chalk-effect">Loser!</p>) : (<p></p>)}
         </div>
     );
 }
